f5843d96a0842545a984ba9e1a7d3cf71b9ab1f9
Lugas90 <Lugas90@mail.ru>
2022-04-26 00:23:55 +0300

    ДЗ урока 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        int i = 1;
+        while(i <= 10){
+            System.out.print (i + " ");
+            i++;
+        }
+        System.out.println(" ");
+        for (int q = 10; q>= 1; q--){
+            System.out.print (q + " ");
+        }
+        System.out.println(" ");
+        // Задание 2
+        for (int day = 3; day <= 31; day = day + 7 ){
+            System.out.println ("Сегодня пятница " + day + " число. Необходимо подготовить отчет.");
+        }
+        // Задание 3
+        int beforeOurYear = 1822;
+        int afterOurYear = 2122;
+        for (int startComet = 0; startComet <= afterOurYear ; startComet = startComet + 79){
+            if (startComet <= beforeOurYear){
+                continue;
+            }
+            System.out.println (startComet);
+
+        }
     }
 }
+
+
+
